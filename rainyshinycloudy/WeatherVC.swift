21dc43af0878ff67da2fecca5bb31760aa3d6b43
Leonardo de Albuquerque Gouveia <leogouv@gmail.com>
2017-08-11 11:18:53 -0300

    iOS Practice - Implements design

---
+++ rainyshinycloudy/WeatherVC.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  rainyshinycloudy
+//
+//  Created by Leonardo Gouveia on 11/08/17.
+//  Copyright © 2017 Leonardo Gouveia. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+    
+
+}
+
